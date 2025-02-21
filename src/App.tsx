eb74b54b67c9153a2058c6f8e36277de86848f9d
AndreGChang <FireWitch2003@outlook.com>
2025-02-20 21:04:26 -0300

    aaa

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,6 @@ function App() {
             <h1>GET REAL!!</h1>
           </div>
         </div>
-        <div className='minecraft-site'>
-          <iframe
-            title="Get Real"
-            src="https://www.minecraft.net/pt-br"
-            width="480"
-            height="270"
-            frameBorder="0"
-            className="giphy-embed"
-            allowFullScreen />
-
-        </div>
       </div>
     </>
   )
